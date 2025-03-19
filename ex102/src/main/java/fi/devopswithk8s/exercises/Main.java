aabf137137412dc88a8088259cc398b23d0d6db7
dilanSachi <dilansachinthanos@gmail.com>
2025-03-19 21:32:05 +0200

    Add deployment.yaml files

---
+++ ex102/src/main/java/fi/devopswithk8s/exercises/Main.java
@@ -0,0 +1,31 @@
+package fi.devopswithk8s.exercises;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started in port 3000");
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException
+        {
+            String response = "Hello, this is a simple HTTP server response!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}

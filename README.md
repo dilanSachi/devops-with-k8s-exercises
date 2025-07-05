f930e396b20692bac27f0b4d61f0010a3f840554
dilanSachi <dilansachinthanos@gmail.com>
2025-07-05 23:05:00 +0300

    Update log logic and readme file

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## DevOps with Kubernetes - Exercises
 
-This repo contains the exercises and projects done in the course [DevOps with Kubernetes](https://devopswithkubernetes.com/) offered by University of Helsinki
+This repo contains the exercises and projects done in the course [DevOps with Kubernetes](https://devopswithkubernetes.com/) offered by University of Helsinki. \
+
+### Chapter 02
+- [1.1.](/ex101)
+- [1.2.](/ex102)
+- [1.3.](/ex103)
+- [1.4.](/ex104)
--- ex101/src/main/java/fi/devopswithk8s/exercises/RandomStringGenerator.java
+++ ex101/src/main/java/fi/devopswithk8s/exercises/RandomStringGenerator.java
@@ -1,15 +1,17 @@
 package fi.devopswithk8s.exercises;
 
+import java.text.SimpleDateFormat;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class RandomStringGenerator {
     static Logger logger = Logger.getLogger(RandomStringGenerator.class.getName());
+    final static String randomString = UUID.randomUUID().toString();
 
     public static void main(String[] args) throws InterruptedException {
         while (true) {
-            logger.log(Level.INFO, UUID.randomUUID().toString());
+            logger.log(Level.INFO, new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new java.util.Date()) + ": " + randomString);
             Thread.sleep(5000);
         }
     }

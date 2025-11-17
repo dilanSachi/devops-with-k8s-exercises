40c00895888368d2071061051b79938b8af6cc6d
dilanSachi <dilansachinthanos@gmail.com>
2025-11-17 23:02:51 +0200

    Add ex201

---
+++ ex201/README.md
@@ -0,0 +1,14 @@
+## Ex2.01
+
+### How to run
+#### Build & run locally
+* Execute `./gradle clean build` inside the `ex201/log_reader` directory. This will build the log reader java project, create a jar and then will create a docker image with the jar file encapsulated.
+* Execute `./gradle clean build` inside the `ex201/ping_pong_application` directory. This will build the ping pong application java project, create a jar and then will create a docker image with the jar file encapsulated.
+* If you are using k3d, import the 2 docker images into the registry by running `k3d image import dilansachi/devopswk8s-ex201-log-reader:1.0.0` and `k3d image import dilansachi/devopswk8s-ex201-ping-pong-application:1.0.0`.
+* Execute `kubectl apply -f manifests/` in the `ex201` directory to deploy the pod with the 2 containers.
+#### Run using image in DockerHub
+* Execute `kubectl apply -f manifests/` to deploy the application.
+
+---
+
+* Server would be available in http://localhost:8081 and http://localhost:8081/pingpong.
---
+++ ex201/log_reader/src/main/java/fi/devopswithk8s/exercises/LogReader.java
@@ -0,0 +1,55 @@
+package fi.devopswithk8s.exercises;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+
+public class LogReader {
+
+    final static String randomString = UUID.randomUUID().toString();
+
+    public static void main(String[] args) throws IOException {
+        int port = 3001;
+        try {
+            port = Integer.parseInt(System.getenv("PORT"));
+        } catch (NumberFormatException e) {
+            System.out.println("PORT variable not found. Starting on default port " + port);
+        }
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started in port " + port);
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String counter = "0";
+            try {
+                HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://ex201-svc:2346/pings")).GET().build();
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+                System.out.println("Received num pings from ping pong app: " + response.body());
+                counter = response.body();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            String response = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new java.util.Date()) + ": " + randomString + "\n" + "Ping / Pongs: " + counter;
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}

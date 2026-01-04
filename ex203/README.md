93e680b77474bc884de1060eb1a67b5b2b9d9fb2
Dilan Sachintha <dilansachinthanos@gmail.com>
2026-01-04 12:38:41 +0200

    Fix 2.03

--- ex203/README.md
+++ ex203/README.md
@@ -4,9 +4,9 @@
 #### Build & run locally
 * Execute `./gradle clean build` inside the `ex203/log_reader` directory. This will build the log reader java project, create a jar and then will create a docker image with the jar file encapsulated.
 * Execute `./gradle clean build` inside the `ex203/ping_pong_application` directory. This will build the ping pong application java project, create a jar and then will create a docker image with the jar file encapsulated.
-* Create a namespace `exercises` by `kubectl create namespace exercises`.
-* If you are using k3d, import the 2 docker images into the registry by running `k3d image import dilansachi/devopswk8s-ex203-log-reader:1.0.0` and `k3d image import dilansachi/devopswk8s-ex203-ping-pong-application:1.0.0`.
-* Execute `kubectl apply -f manifests/` in the `ex203` directory to deploy the pod with the 2 containers.
+* Execute `./gradle clean build` inside the `ex203/log_writer` directory. This will build the log writer java project, create a jar and then will create a docker image with the jar file encapsulated.
+* If you are using k3d, import the 2 docker images into the registry by running `k3d image import dilansachi/devopswk8s-ex203-log-reader:1.0.0`, `k3d image import dilansachi/devopswk8s-ex203-log-writer:1.0.0` and `k3d image import dilansachi/devopswk8s-ex203-ping-pong-application:1.0.0`.
+* Execute `kubectl apply -f manifests/` in the `ex203` directory to deploy the pods with the 3 containers.
 #### Run using image in DockerHub
 * Execute `kubectl apply -f manifests/` to deploy the application.
 
--- ex203/log_reader/src/main/java/fi/devopswithk8s/exercises/LogReader.java
+++ ex203/log_reader/src/main/java/fi/devopswithk8s/exercises/LogReader.java
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -37,15 +38,18 @@ public class LogReader {
         public void handle(HttpExchange exchange) throws IOException {
             String counter = "0";
             try {
-                HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://ex203-svc:2346/pings")).GET().build();
+                HttpRequest request = HttpRequest.newBuilder().uri(new URI("http://ex203-ping-pong-app-svc:2346/pings")).GET().build();
                 HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
                 System.out.println("Received num pings from ping pong app: " + response.body());
                 counter = response.body();
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            FileInputStream fileInputStream = new FileInputStream("/usr/src/app/files/log.txt");
+            String logContent = new String(fileInputStream.readAllBytes());
+            fileInputStream.close();
 
-            String response = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new java.util.Date()) + ": " + randomString + "\n" + "Ping / Pongs: " + counter;
+            String response = logContent + "Ping / Pongs: " + counter;
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());

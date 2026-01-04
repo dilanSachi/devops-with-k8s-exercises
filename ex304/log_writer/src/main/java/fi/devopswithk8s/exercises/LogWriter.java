f8eb4be38bcadec3202cae6356d4f2711d7ef9e4
Dilan Sachintha <dilansachinthanos@gmail.com>
2026-01-04 17:08:15 +0200

    Fix 3.04

---
+++ ex304/log_writer/src/main/java/fi/devopswithk8s/exercises/LogWriter.java
@@ -0,0 +1,25 @@
+package fi.devopswithk8s.exercises;
+
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LogWriter {
+
+    static Logger logger = Logger.getLogger(LogWriter.class.getName());
+    final static String randomString = UUID.randomUUID().toString();
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        while (true) {
+            String text = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(new java.util.Date()) + ": " + randomString + "\n";
+            logger.log(Level.INFO, text);
+            FileOutputStream outputStream = new FileOutputStream("/usr/src/app/files/log.txt", false);
+            byte[] strToBytes = text.getBytes();
+            outputStream.write(strToBytes);
+            outputStream.close();
+            Thread.sleep(5000);
+        }
+    }
+}

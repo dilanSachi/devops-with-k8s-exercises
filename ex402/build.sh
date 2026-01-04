3067efc7539f62ae1bf31bf4fca78e7efb7d69c4
Dilan Sachintha <dilansachinthanos@gmail.com>
2026-01-05 00:13:22 +0200

    Add ex402

---
+++ ex402/build.sh
@@ -0,0 +1,42 @@
+SKIP_LOCAL_IMAGE_BUILD="$1"
+LOG_READER_IMAGE_TAG="$2"
+PING_PONG_APP_IMAGE_TAG="$3"
+LOG_WRITER_IMAGE_TAG="$4"
+
+if [[ -n "$LOG_READER_IMAGE_TAG" ]]
+then
+  echo "Build image tag received as input: $LOG_READER_IMAGE_TAG"
+else
+    LOG_READER_IMAGE_TAG="dilansachi/devopswk8s-ex402-log-reader:1.0.0"
+    echo "Using default image tag: $LOG_READER_IMAGE_TAG"
+fi
+
+if [[ -n "$PING_PONG_APP_IMAGE_TAG" ]]
+then
+  echo "Build image tag received as input: $PING_PONG_APP_IMAGE_TAG"
+else
+    PING_PONG_APP_IMAGE_TAG="dilansachi/devopswk8s-ex402-ping-pong-application:1.0.0"
+    echo "Using default image tag: $PING_PONG_APP_IMAGE_TAG"
+fi
+
+
+if [[ -n "$LOG_WRITER_IMAGE_TAG" ]]
+then
+  echo "Build image tag received as input: $LOG_WRITER_IMAGE_TAG"
+else
+    LOG_WRITER_IMAGE_TAG="dilansachi/devopswk8s-ex402-log-writer:1.0.0"
+    echo "Using default image tag: $LOG_WRITER_IMAGE_TAG"
+fi
+
+if [[ -n "$SKIP_LOCAL_IMAGE_BUILD" ]]
+then
+  echo "SKIP_LOCAL_IMAGE_BUILD: $SKIP_LOCAL_IMAGE_BUILD"
+else
+    SKIP_LOCAL_IMAGE_BUILD="0"
+    echo "Building local images..."
+fi
+
+DIR=$(pwd)
+cd ${DIR}/log_reader && ./gradlew clean build -Pimage_tag="$LOG_READER_IMAGE_TAG" -Pskip_local_image_build=$SKIP_LOCAL_IMAGE_BUILD
+cd ${DIR}/log_writer && ./gradlew clean build -Pimage_tag="$LOG_WRITER_IMAGE_TAG" -Pskip_local_image_build=$SKIP_LOCAL_IMAGE_BUILD
+cd ${DIR}/ping_pong_application && ./gradlew clean build  -Pimage_tag="$PING_PONG_APP_IMAGE_TAG" -Pskip_local_image_build=$SKIP_LOCAL_IMAGE_BUILD
